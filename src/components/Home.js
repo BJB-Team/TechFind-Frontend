ce01beb1cd8e238ae8a2fbc3b2a098638405a43b
benaitcheson <benaitcheson@hotmail.com>
2021-12-26 17:53:02 +1000

    fixed up some linting stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useGlobalState } from '../utils/stateContext'
 
 const Home = () =>{
-  const{store, dispatch} = useGlobalState()
+  const{ store, dispatch } = useGlobalState()
+  const { loggedInUser } = store
 
   return (
     <div>
@@ -11,7 +12,26 @@ const Home = () =>{
         <h1>Your search ends</h1>
         <h1>Here!</h1>
         <p>TechFind! The number one IT platform for Job seekers and companies looking for the best talent.</p>
-        <button>Find Talent!</button>
+
+        {loggedInUser?
+          <>
+            <Link to = "/" onClick ={  }> Find Talent!</Link>
+            <Link to = "/" onClick ={  }> Find Your Career!</Link>
+          </>
+          :
+          <>
+            <Link to ="/signup">Sign up</Link>
+            <Link to ="/login">Login</Link>
+          </>
+        }
+
+
+
+
+
+        {/* If user logged in take them to their homepage otherwise take them to the sign up page */}
+        <button>Find Talent!</button> 
+        {/* If user logged in take them to their homepage otherwise take them to the sign up page */}
         <button>Find Your Career!</button>
       </div>
     </div>   
