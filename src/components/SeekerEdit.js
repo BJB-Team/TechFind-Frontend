8ba7bab476388504e14d4f0d92de526cc19faf57
Ben Aitcheson <benaitcheson@hotmail.com>
2021-12-30 09:59:40 +1000

    add-react-for-profile-pages

--- src/components/SeekerEdit.js
+++ src/components/SeekerEdit.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import { useGlobalState } from '../utils/stateContext'
 
-const Seeker = () => {
+const SeekerEdit = () => {
+    const{ store, dispatch } = useGlobalState()
+    const { loggedInUser } = store
 
     return (
-        
+        <div>
+            <h2>Profile</h2>
+            <h4>Company Name:</h4>
+            {/* Edit company name here */}
+
+            <h4>Company Username:</h4>
+            {/* Edit company name here */}
+
+            <h4>Company E-Mail:</h4>
+            {/* Edit company name here */}
+
+            <h4>Company Phone Number:</h4>
+            {/* Edit company name here */}
+
+            <h4>Company Website:</h4>
+            {/* Edit company name here */}
+
+            <h4>About the Company:</h4>
+            {/* Edit company name here */}
+
+            <Link to = "/"><button>Save</button></Link>
+
+        </div>
     )
 }
+
+export default SeekerEdit
---
+++ src/components/SeekerHome.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import { useGlobalState } from '../utils/stateContext'
+
+const SeekerHome = () => {
+    const{ store, dispatch } = useGlobalState()
+    const { loggedInUser } = store
+
+    return (
+        <div>
+            <h2>Profile</h2>
+            <h4>Company Name:</h4>
+            {/* Access company name here */}
+
+            <h4>Company Username:</h4>
+            {/* Access company name here */}
+
+            <h4>Company E-Mail:</h4>
+            {/* Access company name here */}
+
+            <h4>Company Phone Number:</h4>
+            {/* Access company name here */}
+
+            <h4>Company Website:</h4>
+            {/* Access company name here */}
+
+            <h4>About the Company:</h4>
+            {/* Access company name here */}
+
+            <Link to = "/"><button>Edit Account</button></Link>
+            
+            {/* Make button to delete account */}
+        </div>
+    )
+}
+
+export default SeekerHome
