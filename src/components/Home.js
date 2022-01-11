68404c6ed75e360db3c5ae7cc97e5e1434ac1198
Blaise122 <70517861+Blaise122@users.noreply.github.com>
2022-01-11 13:21:51 +1000

    fixing

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
 
   return (
     <div>
-      <Container>
+      {/* <Container>
         <LeftSide>
           <Header>Your search ends</Header>
           <Header>Here!</Header>
@@ -22,6 +22,29 @@ const Home = () => {
         {/* If the user is logged in as a company they should only be able to see 'Find Talent!' */}
         
         {/* If the user is  logged it will take them to job creation else goes to sign up first*/}
+        {/* <Link to = "/companysignup" > <button className="btn btn-success btn-lg">Find Talent!</button> </Link> */}
+
+        {/*Takes the user to the job listing page doesn't matter if they are logged in or not*/}
+        {/* <Link to = "/job-listings" > <button className="btn btn-success btn-lg">Find Your Career!</button></Link> */}
+
+        {/* <Link to = "/company-job-list" > <button className="btn btn-success btn-lg">my Job List</button></Link> */}
+          
+        {/* <Link to = "/company-job-list" > <button className="btn btn-success btn-lg">my Job List</button></Link>  */}
+
+        {/* </LeftSide> */}
+      {/* </Container> */} 
+
+      <div className="container">
+      <div className="jumbotron">
+        <h1 className="page-header">Your search ends</h1>
+        <h1>Here!</h1>
+        <p>TechFind! The number one IT platform for Job seekers and companies looking for the best talent.</p>
+
+        {/* If the user isn't logged in should display both  */}
+        {/* If the user is logged in as a job seekers they should only be able to see 'Find Your Career!' */}
+        {/* If the user is logged in as a company they should only be able to see 'Find Talent!' */}
+        
+        {/* If the user is  logged it will take them to job creation else goes to sign up first*/}
         <Link to = "/companysignup" > <button className="btn btn-success btn-lg">Find Talent!</button> </Link>
 
         {/*Takes the user to the job listing page doesn't matter if they are logged in or not*/}
@@ -29,10 +52,9 @@ const Home = () => {
 
         <Link to = "/company-job-list" > <button className="btn btn-success btn-lg">my Job List</button></Link>
           
-        <Link to = "/company-job-list" > <button className="btn btn-success btn-lg">my Job List</button></Link> 
+      </div>
+    </div> 
 
-        </LeftSide>
-      </Container>
     </div>
   );
 };
