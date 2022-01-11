597a13efdfeeb3a16760e19566f5ba171d25c018
Blaise122 <70517861+Blaise122@users.noreply.github.com>
2022-01-11 12:40:30 +1000

    made some changes in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,45 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import { useGlobalState } from '../utils/stateContext'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useGlobalState } from "../utils/stateContext";
 
-const Home = () =>{
-  const{ store, dispatch } = useGlobalState()
-  const { loggedInUser } = store
+const Home = () => {
+  const { store, dispatch } = useGlobalState();
+  const { loggedInUser } = store;
 
   return (
-    <div className="container">
-      <div className="jumbotron">
-        <h1 className="page-header">Your search ends</h1>
-        <h1>Here!</h1>
-        <p>TechFind! The number one IT platform for Job seekers and companies looking for the best talent.</p>
+    <div>
+      <Container>
+        <LeftSide>
+          <Header>Your search ends</Header>
+          <Header>Here!</Header>
+          <p>
+            TechFind! The number one IT platform for Job seekers and companies
+            looking for the best talent.
+          </p>
 
-        {/* If the user isn't logged in should display both  */}
-        {/* If the user is logged in as a job seekers they should only be able to see 'Find Your Career!' */}
-        {/* If the user is logged in as a company they should only be able to see 'Find Talent!' */}
-        
-        {/* If the user is  logged it will take them to job creation else goes to sign up first*/}
-        <Link to = "/seekersignup" > <button className="btn btn-success btn-lg">Find Talent!</button> </Link>
+          {/* If the user isn't logged in should display both  */}
+          {/* If the user is logged in as a job seekers they should only be able to see 'Find Your Career!' */}
+          {/* If the user is logged in as a company they should only be able to see 'Find Talent!' */}
 
-        {/*Takes the user to the job listing page doesn't matter if they are logged in or not*/}
-        <Link to = "/job-listings" > <button className="btn btn-success btn-lg">Find Your Career!</button></Link>
-          
-      </div>
-      <div className="sideContainer"> 
+          {/* If the user is  logged it will take them to job creation else goes to sign up first*/}
+          <Link to="/seekersignup">
+            {" "}
+            <button className="btn btn-success btn-lg">
+              Find Talent!
+            </button>{" "}
+          </Link>
+
+          {/*Takes the user to the job listing page doesn't matter if they are logged in or not*/}
+          <Link to="/job-listings">
+            {" "}
+            <button className="btn btn-success btn-lg">
+              Find Your Career!
+            </button>
+          </Link>
+          {/* <Link to = "/company-job-list" > <button className="btn btn-success btn-lg">my Job List</button></Link> */}
+        </LeftSide>
+      </Container>
     </div>
-    </div>   
-    
-  )
-}
-
-export default Home
+  );
+};
+export default Home;
