8af44a5f0760fe0b9004d10c258577059ba09d15
Ben Aitcheson <benaitcheson@hotmail.com>
2022-01-15 18:25:32 +1000

    testing still not working (#42)
    Co-authored-by: Jack Kirkwood <jkirky82@gmail.com>

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,22 +1,14 @@
-import { configure, shallow, mount } from 'enzyme';
+/**
+ * @jest-environment jsdom
+ */
+
 import renderer from 'react-test-renderer';
 import React from 'react';
+import ReactDom from 'react-dom';
 import { Login } from './login';
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { isTSAnyKeyword } from '@babel/types';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-
-describe('Login Component', () => {
-  it('should render properly', () => {
-    const tree = renderer.create(<Login />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('should render an email input tag', () => {
-    const wrapper = shallow(<Login />);
-    expect(wrapper.find('input[name="email"]').exists()).toBe(true);
-  });
-
-});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDom.render(<Login/>, div)
+})
