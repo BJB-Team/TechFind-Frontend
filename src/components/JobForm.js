ada7fb5ea3e450524e84823da846d6c34ce55798
Jack Kirkwood <jkirky82@gmail.com>
2022-01-17 17:50:33 +1000

    fixing some bugs

--- src/components/JobForm.js
+++ src/components/JobForm.js
@@ -40,64 +40,66 @@ const JobForm = () => {
 
 
   return (
-    <ListingContainer>
-      <Heading>Create Job Listing</Heading>
-      <CardContainer>
-      <JobFormCreate onSubmit={handleSubmit}>
-    
-        <ProfileLabel>Title:</ProfileLabel>
-        <input type="text" name="title" id="title" value={formData.title} required onChange={handleFormData} className="form-control-edit"/>
+    <form onSubmit={handleSubmit}>
+      <ListingContainer>
+        <Heading>Create Job Listing</Heading>
+        <CardContainer>
+        <JobFormCreate onSubmit={handleSubmit}>
+      
+          <ProfileLabel>Title:</ProfileLabel>
+          <input type="text" name="title" id="title" value={formData.title} required onChange={handleFormData} className="form-control-edit"/>
 
-        <ProfileLabel>description:</ProfileLabel>
-        <input type="text" name="description" id="description" value={formData.description} required onChange={handleFormData} className="form-control-edit-description"/>
+          <ProfileLabel>description:</ProfileLabel>
+          <input type="text" name="description" id="description" value={formData.description} required onChange={handleFormData} className="form-control-edit-description"/>
 
-        <ProfileLabel>Salary:</ProfileLabel>
-        <input type="number" name="price" id="price" value={formData.price} onChange={handleFormData} className="form-control-edit" />
-          
+          <ProfileLabel>Salary:</ProfileLabel>
+          <input type="number" name="price" id="price" value={formData.price} onChange={handleFormData} className="form-control-edit" />
+            
 
-        <ProfileLabel>Job Level:</ProfileLabel>
-        <select
-						value={formData.job_level_id} required
-						onChange={(e) =>
-							setFormData({
-								...formData,
-								job_level_id: e.target.value
-							})
-						}
-					>
-						<option></option>
-						{jobLevel.map((o, index) => (
-							<option key={index} value={o.id}>
-								{o.name}
-							</option>
-						))}
-					</select>
-          <ProfileLabel>Job Type:</ProfileLabel>
+          <ProfileLabel>Job Level:</ProfileLabel>
           <select
-						value={formData.job_type_id} required
-						onChange={(e) =>
-							setFormData({
-								...formData,
-								job_type_id: e.target.value,
-							})
-						}
-					>
-						<option></option>
-						{jobType.map((o, index) => (
-							<option key={index} value={o.id}>
-								{o.name}
-							</option>
-						))}
-          </select>    
-          
-          <br/>
-          
+              value={formData.job_level_id} required
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  job_level_id: e.target.value
+                })
+              }
+            >
+              <option></option>
+              {jobLevel.map((o, index) => (
+                <option key={index} value={o.id}>
+                  {o.name}
+                </option>
+              ))}
+            </select>
+            <ProfileLabel>Job Type:</ProfileLabel>
+            <select
+              value={formData.job_type_id} required
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  job_type_id: e.target.value,
+                })
+              }
+            >
+              <option></option>
+              {jobType.map((o, index) => (
+                <option key={index} value={o.id}>
+                  {o.name}
+                </option>
+              ))}
+            </select>    
+            
+            <br/>
+            
 
-          <input type="submit" value="Submit Form" />
+            <input type="submit" value="Submit Form" />
           
-      </JobFormCreate>
-      </CardContainer>
-    </ListingContainer>   
+        </JobFormCreate>
+        </CardContainer>
+      </ListingContainer>   
+      </form>
   )
 }
 
