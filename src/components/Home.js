112683f83600d9dc1703b3b06ecd8bf6bbee1b27
Ben Aitcheson <benaitcheson@hotmail.com>
2022-01-17 23:52:10 +1000

    Revert "More styling (#65)" (#66)
    This reverts commit edd0df358a07b7f9cc39df96da54c4b1b5868bd4.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useGlobalState } from "../utils/stateContext";
 import HomeImage from '../Assets/HomeImage.png';
-import {
-  InnerContainer, RightSection,ButtonContainer, HomeButton,
-  MainContainer, HomeHeader1, HomeHeader2, Paragraph
-} from '../styles/General'
 
 
 const Home = () => {
@@ -14,21 +10,21 @@ const Home = () => {
 
   return (
     
-  <MainContainer>
-    <InnerContainer>
-    <HomeHeader1>Your Search Ends</HomeHeader1>
-    <HomeHeader2>Here!</HomeHeader2>
-    <Paragraph>TechFind! The number one IT platform for Job seekers and companies looking for the best talent.</Paragraph>
+  <div className="containerh">
+    <div className="jumbotron">
+    <div className="header">Your Search Ends</div>
+    <div className="header1">Here!</div>
+    <p>TechFind! The number one IT platform for Job seekers and companies looking for the best talent.</p>
 
     {/* If the user isn't logged in should display both  */}
     {/* If the user is logged in as a job seekers they should only be able to see 'Find Your Career!' */}
     {/* If the user is logged in as a company they should only be able to see 'Find Talent!' */}
     
     {/* If the user is  logged it will take them to job creation else goes to sign up first*/}
-    <ButtonContainer>
+    
     {seeker == "false" && loggedInUser ?
       <>
-        <Link to = "/create-job" > <HomeButton>Find Talent</HomeButton> </Link>
+        <Link to = "/create-job" > <button className="homebutton">Find Talent</button> </Link>
       </>
       :
       <>
@@ -39,34 +35,49 @@ const Home = () => {
     <>
 
     </>
-          :
-    
+    :
     <>
-      <Link to = "/companysignup" > <HomeButton>Find Talent</HomeButton> </Link>
+      <Link to = "/companysignup" > <button className="homebutton">Find Talent</button> </Link>
     </>
   }
     
 
     {/*Takes the user to the job listing page doesn't matter if they are logged in or not*/}
-    <Link to = "/job-listings" > <HomeButton>Find Your Career</HomeButton></Link>
+    <Link to = "/job-listings" > <button className="homebutton">Find Your Career</button></Link>
 
-        <Link to="/company-job-list" > <HomeButton>My Job List</HomeButton></Link>
-    
-        </ButtonContainer> 
-        
-    </InnerContainer>
+    <Link to = "/company-job-list" > <button className="homebutton">My Job List</button></Link>
+      
+    </div>
 
-    <RightSection>
+    <div className="rightSection">
     <img src={HomeImage} />
-    </RightSection> 
+    </div> 
     
-  </MainContainer>  
+  </div>  
 
   );
 };
 
 
-
+//styles
+// const Container = styled.div`
+//   display: flex;
+//   height: 90vh;
+//   align-items: center;
+//   justify-content: center;
+//   @media only screen and (max-width: 960px) {
+//     flex-direction: column-reverse;
+//     width: 100%;
+//     // height: 100%;
+//     margin: 3rem 2rem;
+//   }
+//   @media only screen and (max-width: 768px) {
+//     flex-direction: column-reverse;
+//     width: 80%;
+//     // height: 100%;
+//     margin: 1rem 2rem;
+//   }
+// `;
 
 
 export default Home;
